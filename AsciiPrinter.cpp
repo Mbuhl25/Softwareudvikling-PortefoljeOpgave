b1dd3d6441d9437b7a18ec89a839ec2178169081
Mbuhl25 <mbuhl25@student.sdu.dk>
2026-03-21 19:01:20 +0100

    Changed The print to have a variable screen length

--- AsciiPrinter.cpp
+++ AsciiPrinter.cpp
@@ -4,28 +4,40 @@
 #include <iomanip>
 #include "AsciiPrinter.h"
 
-AsciiPrinter::AsciiPrinter() {}
+AsciiPrinter::AsciiPrinter() {
+    screenWidth = 51;
+    if (screenWidth < 51 || screenWidth % 2 == 0) {
+        std::cout << "ScreenWidth is either too small ( < 51 ), or even, which is illegal";
+        exit(1);
+    }
+}
 
 void AsciiPrinter::updateMonsters(std::vector<std::string> _friendlyMonster, std::vector<std::string> _enemyMonster) {
     friendlyMonster = _friendlyMonster;
     enemyMonster = _enemyMonster;
+    
 }
 
 void AsciiPrinter::printScreen() {
-    std::cout << "+============================================================+" << std::endl;
-    std::cout << "|" << std::setw(61) << "|" << std::endl;
+    std::cout << (screenWidth-20-20-5-5)/2 << std::endl;
+    std::cout << "+"; for (int i = 1; i < screenWidth; ++i) {std::cout << "=";}; std::cout << "+" << std::endl;
+    
+    std::cout << "|" << std::setw(screenWidth) << "|" << std::endl;
     for (int i = 0; i < friendlyMonster.size(); ++i){
-        std::cout << std::left << std::setw(5) << "|"
+        std::cout << std::left
+              << std::setw(5) << "|"
               << std::setw(20) << friendlyMonster[i]
-              << std::setw(5)<< "" << std::setw(2)<< "VS" << std::setw(5)<< ""
+              << std::setw((screenWidth-20-20-5-5)/2)<< ""
+              << std::setw(2)<< "VS"
+              << std::setw((screenWidth-20-20-5-5)/2)<< ""
               << std::setw(20) << enemyMonster[i]
               << std::right << std::setw(5) << "|" << std::endl;
     }
-    std::cout << "|" << std::setw(61) << "|" << std::endl;
-    std::cout << "+============================================================+" << std::endl;
-    std::cout << "|" << std::setw(61) << "|" << std::endl;
-    std::cout << "|" << std::setw(61) << "|" << std::endl;
-    std::cout << "+============================================================+" << std::endl;
+    std::cout << "|" << std::setw(screenWidth) << "|" << std::endl;
+    std::cout << "+"; for (int i = 1; i < screenWidth; ++i) {std::cout << "=";}; std::cout << "+" << std::endl;
+    std::cout << "|" << std::setw(screenWidth) << "|" << std::endl;
+    std::cout << "|" << std::setw(screenWidth) << "|" << std::endl;
+    std::cout << "+"; for (int i = 1; i < screenWidth; ++i) {std::cout << "=";}; std::cout << "+" << std::endl;
 }
 
 AsciiPrinter::~AsciiPrinter() {}
